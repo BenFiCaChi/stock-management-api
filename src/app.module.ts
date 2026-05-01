91d37549cd07151af06792c040e841117251b522
BenFiCaChi <benficachi@gmail.com>
2026-05-01 16:06:38 +0100

    feat: implement warehouse module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ProductModule } from './modules/product/product.module';
 import { PrismaService } from './modules/prisma/prisma.service';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { MovementModule } from './modules/movement/movement.module';
+import { WarehouseModule } from './modules/warehouse/warehouse.module';
 
 @Module({
-  imports: [ProductModule, PrismaModule, MovementModule],
+  imports: [ProductModule, PrismaModule, MovementModule, WarehouseModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
